b2a2035aa8d96b8b434af1e444f6ddf71d07b528
jaxknz <19jackleonard99@gmail.com\>
2021-01-08 22:30:54 +1300

    Favourite form of transportation added

--- PartDA2/src/main.java
+++ PartDA2/src/main.java
@@ -3,6 +3,7 @@ public class main {
 	public main(String[]args) {
 		System.out.println("Hi, my name is Jack");
 		System.out.println("My favourite colour is blue");
+		System.out.println("My favourite form of transportation is car");
 	}
 
 }
