0218b05d2929ef1b7e221ea00d98cd1477675107
jaxknz <19jackleonard99@gmail.com\>
2021-01-08 22:35:09 +1300

    Best day of the week added

--- PartDA2/src/main.java
+++ PartDA2/src/main.java
@@ -4,6 +4,7 @@ public class main {
 		System.out.println("Hi, my name is Jack");
 		System.out.println("My favourite colour is blue");
 		System.out.println("My favourite form of transportation is car");
+		System.out.println("The best day of the week is friday");
 	}
 
 }
